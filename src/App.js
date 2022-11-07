0cc13c36be936936aa9b8f885213f92835698b0a
Markus <markus.kloepper@gmx.net>
2022-11-07 10:14:54 +0100

    finished data import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import data from "./data";
+
+
 
 export default function App() {
+const cardData = data.map(item => {
+  return (
+    <Card 
+      key={item.id}
+      {...item}
+    />
+  )
+});
+
   return (
     <div className="app-wrapper">
       <Navbar />
       <div className="cards-wrapper">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {cardData}
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,36 +1,31 @@
-import { hover } from "@testing-library/user-event/dist/hover";
 import locationImg from "./images/location.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <img
-        src="https://source.unsplash.com/JmuyB_LibRo"
-        alt="japan"
+        src={props.imageURL}
+        alt="img of loc"
         className="card-main-img"
       />
       <div className="card-infos">
         <div className="card-infos-header flex">
           <div className=" card-infos-loc flex">
             <img src={locationImg} alt="loc"></img>
-            <span className="card-info-country">Australia</span>
+            <span className="card-info-country">{props.location}</span>
           </div>
           <a 
-          href="https://www.bing.com/maps?q=google+Maps+sydney+opera+house&FORM=HDRSC4&cp=-33.869721%7E151.212022&lvl=16.0"
+          href={props.linkMaps}
           className="link"
           >
             View on Google Maps
           </a>
         </div>
-        <h1>Sydney Opera House</h1>
-        <h3>27 May, 2021 - 8 Jun, 2021</h3>
-        <p>
-          The Sydney Opera House is a multi-venue performing arts centre in
-          Sydney. Located on the banks of the Sydney Harbour, it is often
-          regarded as one of the 20th century's most famous and distinctive
-          buildings
-        </p>
+        <h1>{props.title}</h1>
+        <h3>{props.startDate} - {props.endDate}</h3>
+        <p>{props.description}</p>
       </div>
     </div>
   );
 }
+ 
